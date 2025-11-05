d0e285f35ab8ca4eb27a8c6138c96c44f9b870c3
Leonardo Ferreira <leonardoff.eng@gmail.com>
2025-11-05 19:42:18 +0100

    tidying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ConvexClientProvider } from "./convex-client-provider";
 
 export const metadata: Metadata = {
   title: "Tech MUC",
